196fee5fa23ccdae6058963f2a6ae48c80981ebe
newptr <newnnew@hotmail.co.th>
2015-12-12 19:52:04 +0700

    Initial check-in of module Game

---
+++ Game/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Game
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string b = Console.ReadLine (); 
+			string a = Console.ReadLine ();
+			Console.WriteLine (b +  "  " + a);
+		}
+	}
+}
