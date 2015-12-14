53d31b1026edd1725d383e3ec209aea2d5e77340
newptr <newnnew@hotmail.co.th>
2015-12-14 20:28:42 +0700

    player1

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-
 namespace Game
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
-		{
-			Console.WriteLine ("test");
+//		public static void Main (string[] args)
+//		{
+//			int a = Console.WriteLine();
+//			int b = Console.WriteLine();
+//			int [][] array = new int[a][b]; 
+//					Console.WriteLine ("test");
+//					Console.WriteLine ("test");
+//				}
+//			}
+//		}
+
+		public static void Main (string[] args) {
+			int tableSizeX = int.Parse(Console.ReadLine()); //5
+			int tableSizeY = int.Parse(Console.ReadLine()); //7
+
+			Random r = new Random();
+			int P1LocationX = r.Next (0, tableSizeX);
+			int P1LocationY = r.Next (0, tableSizeY);
 		}
 	}
 }
