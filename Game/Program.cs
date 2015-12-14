1a3ecc459b9c4a23bbc06d3bb71251c5e040f51e
newptr <newnnew@hotmail.co.th>
2015-12-14 21:00:48 +0700

    player 1 complete

--- Game/Program.cs
+++ Game/Program.cs
@@ -14,13 +14,28 @@ namespace Game
 //			}
 //		}
 
+		static int tableSizeX;
+		static int tableSizeY;
+		static Random r = new Random();
+		
 		public static void Main (string[] args) {
-			int tableSizeX = int.Parse(Console.ReadLine()); //5
-			int tableSizeY = int.Parse(Console.ReadLine()); //7
 
-			Random r = new Random();
-			int P1LocationX = r.Next (0, tableSizeX);
-			int P1LocationY = r.Next (0, tableSizeY);
+			tableSizeX = int.Parse(Console.ReadLine());
+			tableSizeY = int.Parse(Console.ReadLine());
+
+			int[] P1Location = RandomLocation ();
+			PrintLocation( 1, P1Location );
+
+			int[] P2Location = RandomLocation ();
+			PrintLocation( 2, P2Location );
+		}
+
+		public static int[] RandomLocation() {
+			return new int[2]{ r.Next (0, tableSizeX - 1), r.Next (0, tableSizeY - 1) };
+		}
+
+		public static void PrintLocation( int playerId, int[] location ) {
+			Console.WriteLine("player in this world > id: " + playerId + " " + location[0] + " " + location[1]);
 		}
 	}
 }
