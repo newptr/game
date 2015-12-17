f77b521fb41689d17e4a7c264b19cc4a1503ed14
newptr <newnnew@hotmail.co.th>
2015-12-18 00:07:11 +0700

    add Control --if choose up down left Right --if meet impasse

--- Game/Program.cs
+++ Game/Program.cs
@@ -14,14 +14,11 @@ namespace Game
 //			}
 //		}
 
-		static int tableSizeX;
-		static int tableSizeY;
+		static int tableSizeX ;
+		static int tableSizeY ;
 		static Random r = new Random();
 		
 		public static void Main (string[] args) {
-
-			tableSizeX = int.Parse(Console.ReadLine());
-			tableSizeY = int.Parse(Console.ReadLine());
 
 			int[] P1Location = RandomLocation ();
 			PrintLocation( 1, P1Location );
@@ -29,13 +26,26 @@ namespace Game
 			int[] P2Location = RandomLocation ();
 			PrintLocation( 2, P2Location );
 		}
-
 		public static int[] RandomLocation() {
 			return new int[2]{ r.Next (0, tableSizeX - 1), r.Next (0, tableSizeY - 1) };
 		}
 
 		public static void PrintLocation( int playerId, int[] location ) {
 			Console.WriteLine("player in this world > id: " + playerId + " " + location[0] + " " + location[1]);
+		} 
+		Console.WriteLine("Move")
+		public static int[] Up(){
+			return new int[2]{r.Next(0,tableSizeX-1),r.Next (0, tableSizeY)}; 
+		}
+		public static int[] Down(){
+			return new int[2]{r.Next(0,tableSizeX-1),r.Next (0, tableSizeY-2)}; 
+		}
+		public static int[] Left(){
+			return new int[2]{r.Next(0,tableSizeX),r.Next (0, tableSizeY-1)}; 
+		}
+		public static int[] Down(){
+			return new int[2]{r.Next(0,tableSizeX-2),r.Next (0, tableSizeY-1)}; 
+		}
 		}
 	}
 }
