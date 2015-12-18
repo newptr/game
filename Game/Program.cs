221fe355bbde2b5699fcbec4169fd02966574008
newptr <newnnew@hotmail.co.th>
2015-12-18 20:27:37 +0700

    +control

--- Game/Program.cs
+++ Game/Program.cs
@@ -3,49 +3,46 @@ namespace Game
 {
 	class MainClass
 	{
-//		public static void Main (string[] args)
-//		{
-//			int a = Console.WriteLine();
-//			int b = Console.WriteLine();
-//			int [][] array = new int[a][b]; 
-//					Console.WriteLine ("test");
-//					Console.WriteLine ("test");
-//				}
-//			}
-//		}
+			static int tableSizeX ;
+			static int tableSizeY ;
+			static Random r = new Random();
+			
+			public static void Main (string[] args) {
+			tableSizeX = int.Parse(Console.ReadLine ());
+			tableSizeY = int.Parse(Console.ReadLine ());
 
-		static int tableSizeX ;
-		static int tableSizeY ;
-		static Random r = new Random();
-		
-		public static void Main (string[] args) {
+				int[] P1Location = RandomLocation ();
+				PrintLocation( 1, P1Location );
 
-			int[] P1Location = RandomLocation ();
-			PrintLocation( 1, P1Location );
+				int[] P2Location = RandomLocation ();
+				PrintLocation( 2, P2Location );
+			}
+			public static int[] RandomLocation() {
+				return new int[2]{ r.Next (0, tableSizeX - 1), r.Next (0, tableSizeY - 1) };
+			}
+			public static void PrintLocation( int playerId, int[] location ) {
+				Console.WriteLine("player in this world > id: " + playerId + " " + location[0] + " " + location[1]);
+			} 
+			/*public static int Control(){
+				String Control = Console.ReadLine(); 
+				Console.WriteLine("Move"+Control);
+				switch(Control){
+					case "Up":
 
-			int[] P2Location = RandomLocation ();
-			PrintLocation( 2, P2Location );
-		}
-		public static int[] RandomLocation() {
-			return new int[2]{ r.Next (0, tableSizeX - 1), r.Next (0, tableSizeY - 1) };
-		}
-
-		public static void PrintLocation( int playerId, int[] location ) {
-			Console.WriteLine("player in this world > id: " + playerId + " " + location[0] + " " + location[1]);
-		} 
-		Console.WriteLine("Move")
-		public static int[] Up(){
-			return new int[2]{r.Next(0,tableSizeX-1),r.Next (0, tableSizeY)}; 
-		}
-		public static int[] Down(){
-			return new int[2]{r.Next(0,tableSizeX-1),r.Next (0, tableSizeY-2)}; 
-		}
-		public static int[] Left(){
-			return new int[2]{r.Next(0,tableSizeX),r.Next (0, tableSizeY-1)}; 
-		}
-		public static int[] Down(){
-			return new int[2]{r.Next(0,tableSizeX-2),r.Next (0, tableSizeY-1)}; 
-		}
-		}
+					break;
+					case ("Down"):
+					
+					break;
+					case ("Left"):
+					
+					break;
+					case ("Right"):
+					 
+					break;
+			default:
+				Console.WriteLine ("Exit program");
+				break;
+				}
+			}*/
 	}
 }
