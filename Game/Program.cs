b5ef32581265919c8e863c3f30da7b7d07915159
newptr <newnnew@hotmail.co.th>
2015-12-20 20:44:56 +0700

    refactor method

--- Game/Program.cs
+++ Game/Program.cs
@@ -3,46 +3,59 @@ namespace Game
 {
 	class MainClass
 	{
-			static int tableSizeX ;
-			static int tableSizeY ;
-			static Random r = new Random();
-			
-			public static void Main (string[] args) {
+		static int tableSizeX ;
+		static int tableSizeY ;
+		static Random r = new Random();
+		
+		public static void Main (string[] args) {
 			tableSizeX = int.Parse(Console.ReadLine ());
 			tableSizeY = int.Parse(Console.ReadLine ());
 
-				int[] P1Location = RandomLocation ();
-				PrintLocation( 1, P1Location );
+			int[] P1Location = RandomLocation ();
+			PrintLocation( 1, P1Location );
 
-				int[] P2Location = RandomLocation ();
-				PrintLocation( 2, P2Location );
-			}
-			public static int[] RandomLocation() {
-				return new int[2]{ r.Next (0, tableSizeX - 1), r.Next (0, tableSizeY - 1) };
-			}
-			public static void PrintLocation( int playerId, int[] location ) {
-				Console.WriteLine("player in this world > id: " + playerId + " " + location[0] + " " + location[1]);
-			} 
-			/*public static int Control(){
-				String Control = Console.ReadLine(); 
-				Console.WriteLine("Move"+Control);
-				switch(Control){
-					case "Up":
+			int[] P2Location = RandomLocation ();
+			PrintLocation( 2, P2Location );
 
-					break;
-					case ("Down"):
-					
-					break;
-					case ("Left"):
-					
-					break;
-					case ("Right"):
-					 
-					break;
-			default:
+			while (true) {
+				String control = Console.ReadLine ();
+
+				Console.WriteLine ("Move" + control);
+				MoveLocation (control, P1Location);
+				PrintMovedLocation (1, P1Location);
+			}
+		}
+
+		public static int[] RandomLocation() {
+			return new int[2]{ r.Next (0, tableSizeX - 1), r.Next (0, tableSizeY - 1) };
+		}
+
+		public static void PrintLocation( int playerId, int[] location ) {
+			Console.WriteLine("player in this world > id: " + playerId + " " + location[0] + " " + location[1]);
+		}
+
+		public static void PrintMovedLocation( int playerId, int[] location ) {
+			Console.WriteLine("player " + playerId + " moved to" + location[0] + " " + location[1]);
+		}
+
+		public static void MoveLocation( string command, int[] location){
+			switch(command){
+			case "Up":
+				location [1] += 1;
+				break;
+				case "Down":
+				location [1] -= 1;
+				break;
+				case "Left":
+				location [0] -= 1;
+				break;
+				case "Right":
+				location [0] += 1;
+				break;
+				default:
 				Console.WriteLine ("Exit program");
 				break;
-				}
-			}*/
+			}
+		}
 	}
 }
